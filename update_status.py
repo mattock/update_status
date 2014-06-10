c03ee907934b0f059e76a77f72f8a48d2ba2a006
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-06-10 16:57:31 +0300

    Migrated to using update-notifier
    The use of uname and apt-show-versions for detecting if a reboot was needed
    proved to be too fragile. Started using the standard Debian/Ubuntu tools in
    update-notifier-common package to determine how many pending updates and
    security updates there are, as well as whether the computer needs to be
    rebooted. The only minor issue with this approach is that reboot check will not
    work until at least one kernel has been installed after the
    update-notifier-common install.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- update_status.py
+++ update_status.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import apt, re, subprocess
+import apt, re, subprocess, os
 
 class Update_status():
     """Class that models that update status of a system"""
 
     def __init__(self, *args, **kwargs):
 
+        # Executable locations
+        self.un_dir = '/usr/lib/update-notifier'
+        self.apt_check = os.path.join(self.un_dir, 'apt-check')
+        self.reboot_required_file = '/var/run/reboot-required'
+
         # Update the cache before doing anything
         #self.update_apt_cache()
         self.kernel_regex=re.compile("^linux-image-.*$")
 
-        self.upgradable_packages = self.get_upgradable_packages()
+        # Data derived using the Python apt module
+        self.pending_updates = self.get_pending_updates()
         self.kernel_upgradable = self.is_kernel_upgradable()
-        self.running_installed_kernel = self.is_running_installed_kernel()
+
+        # Data derived from update-notifier
+        self.pending_update_count = None
+        self.pending_security_update_count = None
+        self.set_update_counts()
+        self.need_restart = self.does_need_restart()
 
     def update_apt_cache(self):
         """Update apt cache. This only affects the underlying system state, not the object state."""
@@ -23,83 +34,68 @@ class Update_status():
         cache.open(None)
         cache.update()
 
-    def get_upgradable_packages(self):
+    def set_update_counts(self):
+        """Set update counts"""
+        aco = subprocess.check_output(self.apt_check, shell=False, stderr=subprocess.STDOUT)
+        self.pending_update_count = aco.split(';')[0]
+        self.pending_security_update_count = aco.split(';')[1]
+
+    def get_pending_updates(self):
         """Get a list of upgradable package objects"""
 
         cache = apt.Cache()
         cache.open(None)
 
-        upgradable_packages = []
+        pending_updates = []
 
         for key in cache.keys():
             package = cache[key]
 
             if package.is_upgradable:
-                upgradable_packages.append(package)
+                pending_updates.append(package)
 
-        return upgradable_packages
+        return pending_updates
 
     def is_kernel_upgradable(self):
         """Return a boolean value telling whether or not there are pending kernel updates"""
 
         kernel_upgradable = False
 
-        for package in self.upgradable_packages:
+        for package in self.pending_updates:
             if self.kernel_regex.match(package.name):
                 kernel_upgradable = True
 
         return kernel_upgradable
 
-    def is_running_installed_kernel(self):
-        """Determine if the running kernel is different from the installed 
-           kernel"""
+    def get_pending_update_count(self):
+        """Return the number of pending updates"""
+        return self.pending_update_count
 
-        # Extract running kernel version, build and type from uname output
-        uname_output = subprocess.check_output(['uname', '-r'], shell=False)
-        r_version = uname_output.split('-')[0].rstrip('\n')
-        r_build = uname_output.split('-')[1].rstrip('\n')
-        type = uname_output.split('-')[2].rstrip('\n')
+    def get_pending_security_update_count(self):
+        """Return the number of pending security updates"""
+        return self.pending_update_count
 
-        # Construct the kernel package name for the running kernel
-        kernel_package = 'linux-image-{type}'.format(type=type)
-
-        # Convert uname-produced version info into format compatible with 
-        # apt-show-versions output.
-        r_full_version = r_version+"."+r_build
-
-        # apt-show-versions can produce several types of output, all of which we 
-        # need to be able to parse:
-        #
-        # linux-image-virtual/precise upgradeable from 3.2.0.63.75 to 3.2.0.64.76
-        # linux-image-virtual/precise uptodate 3.2.0.64.76
-        # linux-image-virtual not installed
-        #
-        asv_output = subprocess.check_output(['apt-show-versions', '-p', kernel_package], shell=False)
-
-        # It's not always possible to determine the kernel metapackage name from 
-        # uname output. This can happen when using backported kernels,
-        if "not installed" in asv_output:
-            return None
-
-        # This only checks if the running kernel differs from the installed 
-        # kernel, not whether it's older or newer.
-        if not r_full_version in asv_output:
-            return False
-        else:
+    def does_need_restart(self):
+        """Determine if a restart is required. This will only work properly after a kernel has been installed after update-notifier-common installation"""
+        if os.path.isfile(self.reboot_required_file):
             return True
 
-    def upgradable_packages_to_str(self):
+    def pending_updates_to_str(self):
         """Return a string presentation of all upgradable packages"""
 
         updates=""
-        for package in self.upgradable_packages:
+        for package in self.pending_updates:
             updates = updates + package.name + "\n"
 
         return updates
 
-    def update_count_to_str(self):
+    def pending_update_count_to_str(self):
         """Return a string presentation of the upgradable package count"""
-        return str(len(self.upgradable_packages))
+        return str(self.pending_update_count)
+
+    def pending_security_update_count_to_str(self):
+        """Return a string presentation of the upgradable package count"""
+        return str(self.pending_security_update_count)
 
     def kernel_upgradable_to_str(self):
         """Return a string telling whether kernel can be updated"""
@@ -109,40 +105,26 @@ class Update_status():
         else:
             return "No"
 
-    def running_installed_kernel_to_str(self):
-        """Return a string telling if the running kernel is different from the 
-           installed kernel."""
+    def need_restart_to_str(self):
+        """Return a string telling if a restart is required"""
 
-        if self.running_installed_kernel is None:
-            return "Unknown"
-        elif self.running_installed_kernel:
+        if self.need_restart:
             return "Yes"
         else:
             return "No"
 
-    def oneline(self):
-        """One-line representation of the system update status"""
-
-        return "Kernel upgradable: "+self.kernel_upgradable_to_str()+\
-             "\tUpgradable packages: "+self.update_count_to_str()+\
-             "\tRunning installed kernel: "+self.running_installed_kernel_to_str()
-
-    def csv(self):
-        """CSV representation of the system update status"""
-        return self.kernel_upgradable_to_str()+","+self.update_count_to_str()+","+self.running_installed_kernel_to_str()
-
     def __str__(self):
         """Detailed string representation of the system update status"""
 
-        output = "Upgradable packages ("+self.update_count_to_str()+")\n"\
-                  +self.upgradable_packages_to_str()+"\n"\
-                  +"Kernel upgradable: "+self.kernel_upgradable_to_str()
-        return output
+        return "Kernel upgradable: "+self.kernel_upgradable_to_str() +\
+             "\tPending updates: "+self.pending_update_count_to_str() +\
+             "\tPending security updates: "+self.pending_security_update_count_to_str() +\
+             "\tNeeds restart: "+self.need_restart_to_str()
 
 def main():
     """The default action is to print a human-readable, one-line report system's update status"""
     update_status = Update_status()
-    print update_status.oneline()
+    print update_status
 
 if __name__ == '__main__':
     main()
